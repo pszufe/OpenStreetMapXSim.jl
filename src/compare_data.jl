2d1d4b8688ee48a72b9e31294b5ccbe67163ce1f
bartoszpankratz <32309411+bartoszpankratz@users.noreply.github.com>
2018-10-01 02:58:59 +0200

    x

--- src/compare_data.jl
+++ src/compare_data.jl
@@ -13,13 +13,16 @@ using DataFrames
 using Nanocsv
 using GLM
 
-function compare_data(datapath::String,mapfile::String,resultfile::String, testfile::String)
-    map_data = OpenStreetMapX.get_map_data(datapath,mapfile)
+function compare_data(datapath::String,mapfile::String,resultfile::String, testfile::String, counttype::Union{Nothing,String}, save::Bool = true)
+    bounds,nodes,roadways,intersections,network = OSMSim.read_map_file(datapath,mapfile)
     frame = DataFrames.DataFrame(Node_ID = Int[],latitude = Float64[], longitude = Float64[], empirical = Int[],simulation = Int[])
     traffic = Nanocsv.read_csv(joinpath(datapath,testfile))
+    if !isa(counttype,Nothing)
+        traffic = traffic[traffic[:CNTTYPE1] .== counttype,:]
+    end
     traffic_data = Dict()
     for i in 1:nrow(traffic)
-        key = OpenStreetMapX.nearest_node(map_data.nodes,OpenStreetMapX.ENU(OpenStreetMapX.LLA(traffic[:LATITUDE][i],traffic[:LONGITUDE][i]),map_data.bounds),collect(keys(map_data.intersections)))
+        key = OpenStreetMap.nearest_node(nodes,OpenStreetMap.ENU(OpenStreetMap.LLA(traffic[:LATITUDE][i],traffic[:LONGITUDE][i]),bounds),collect(keys(intersections)))
         if haskey(traffic_data, key)
             traffic_data[key][:TRAFFIC1][1] += traffic[i,:TRAFFIC1]
         else
@@ -35,7 +38,14 @@ function compare_data(datapath::String,mapfile::String,resultfile::String, testf
     end
     model = GLM.lm(@formula(simulation ~ empirical - 1),frame)
     frame[:scaled_empirical] = GLM.predict(model)
+    if !isa(counttype,Nothing)
+        save && write_csv(joinpath(datapath,"traffic_data_comparision_$counttype.csv"),frame)
+    else
+        save && Nanocsv.write_csv(joinpath(datapath,"traffic_data_comparision.csv"),frame)
+    end
     return frame, model
 end
 
-dataframe, model = compare_data(datapath,mapfile,sim_results,datafile)
+for count in counttypes
+    frame, regression = compare_data(datapath,mapfile,resultfile,filename, count)
+end
